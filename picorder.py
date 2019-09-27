da97195e61599367545e6bbd87eb3b3e9e705b41
Terry Howald <terry.howald@gmail.com>
2019-09-27 13:40:27 -0400

    Implemented previous chapter button.

--- picorder.py
+++ picorder.py
@@ -13,65 +13,103 @@ Written by Terry Howald
 terry.howald@gmail.com
 """
 
-import vlc
-import sys
+import RPi.GPIO as GPIO
 import time
+import vlc
+
+GPIO_BOARD_PIN_12 = 12
+GPIO_BOARD_PIN_16 = 16
+
+class VideoPlayer:
+    
+    # Constructor
+    def __init__(self):
+        
+        # Setup GPIO
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        GPIO.setup(GPIO_BOARD_PIN_12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(GPIO_BOARD_PIN_16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        
+        # Add event for button push
+        GPIO.add_event_detect(GPIO_BOARD_PIN_12, GPIO.RISING, callback=self.NextChapter_Callback) 
+        GPIO.add_event_detect(GPIO_BOARD_PIN_16, GPIO.RISING, callback=self.PrevChapter_Callback)    
+        
+        # Create instance of VLC and force window to be top level
+        Instance = vlc.Instance(['--video-on-top'])
+        #Instance = vlc.Instance()        
+
+        # Create media player object
+        self.player = Instance.media_player_new()
+
+        # Set media player to fullscreen
+        self.player.set_fullscreen(True)
+
+        # Set path to video to be played
+        Media = Instance.media_new("/home/pi/Desktop/Phantom of the Opera (1943).m4v")
+
+        # Give the media player object the video to be played
+        self.player.set_media(Media)
+        
+
+    def SetupGPIO(self):
+        pass
+        
+    def SetupVideoPlayer(self):
+        pass
+        
+    def Play(self):
+        self.player.play()
+        
+    def Stop(self):
+        self.player.stop()
+        
+    def NextChapter(self):
+        self.player.next_chapter()
+        
+    def PrevChapter(self):
+        self.player.previous_chapter()
+        
+    def NextChapter_Callback(self, channel):
+        GPIO.remove_event_detect(GPIO_BOARD_PIN_12)
+        print("Button was pushed!")
+        
+        # Seek to next chapter in video
+        self.NextChapter()
+        
+        time.sleep(0.25)
+        GPIO.add_event_detect(GPIO_BOARD_PIN_12, GPIO.RISING, callback=self.NextChapter_Callback)  
+        
+    def PrevChapter_Callback(self, channel):
+        GPIO.remove_event_detect(GPIO_BOARD_PIN_16)
+        print("Button was pushed!")
+        
+        # Seek to next chapter in video
+        self.PrevChapter()
+        
+        time.sleep(0.25)
+        GPIO.add_event_detect(GPIO_BOARD_PIN_16, GPIO.RISING, callback=self.PrevChapter_Callback) 
+    
+    # Destructor    
+    def __del__(self):
+        GPIO.cleanup()
 
 def main():
-
-    # Create instance of VLC and force window to be top level
-    Instance = vlc.Instance(['--video-on-top'])
-
-    # Create media player object
-    player = Instance.media_player_new()
-
-    # Set media player to fullscreen
-    player.set_fullscreen(True)
-
-    # Set path to video to be played
-    Media = Instance.media_new("/home/terry/Downloads/Creature from the Black Lagoon (1954).m4v")
-
-    # Give the media player object the video to be played
-    player.set_media(Media)
-
-    # Play video
-    player.play()
-
+    videoplayer = VideoPlayer()
+    
+    videoplayer.Play()
+   
     # Wait for 10 seconds
-    time.sleep(10)
+    #time.sleep(10)
 
     # Set media player to default screen size
-    player.set_fullscreen(False)
-
-    # Wait for enter key press
-    str = ""
-    while str == "":
-        str = sys.stdin.read(1)
-        pass   
-
-    # Once enter key is pressed, pause video
-    player.pause()
-
-    # Wait for 5 seconds
-    time.sleep(5)
-
-    # Seek to next chapter in video
-    player.next_chapter()
-
-    # Play video
-    player.play()
-
-    # Wait for 10 seconds
-    time.sleep(10)
-
-    # Seek to previous chapter in video
-    player.previous_chapter()
-
-    # Wait for 10 seconds
-    time.sleep(10)    
-
+    #player.set_fullscreen(False)    
+    
+    # Wait for Enter press
+    message = input("Press enter to quit\n\n")
+    
     # Stop video and exit program
-    player.stop() 
+    videoplayer.Stop() 
 
 if __name__ == '__main__':
     main()
