54cf00c74511e3fd5a3d942b953aa65a9b793f53
Terry Howald <terry.howald@gmail.com>
2019-09-30 22:12:39 -0400

    Added more button presses

--- picorder.py
+++ picorder.py
@@ -16,9 +16,12 @@ terry.howald@gmail.com
 import RPi.GPIO as GPIO
 import time
 import vlc
+import os
 
 GPIO_BOARD_PIN_12 = 12
 GPIO_BOARD_PIN_16 = 16
+GPIO_BOARD_PIN_18 = 18
+VIDEOS_DIRECTORY = "/home/pi/Desktop/videos"
 
 class VideoPlayer:
     
@@ -30,26 +33,33 @@ class VideoPlayer:
         GPIO.setwarnings(False)
         GPIO.setup(GPIO_BOARD_PIN_12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(GPIO_BOARD_PIN_16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(GPIO_BOARD_PIN_18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)        
         
         # Add event for button push
         GPIO.add_event_detect(GPIO_BOARD_PIN_12, GPIO.RISING, callback=self.NextChapter_Callback) 
-        GPIO.add_event_detect(GPIO_BOARD_PIN_16, GPIO.RISING, callback=self.PrevChapter_Callback)    
+        GPIO.add_event_detect(GPIO_BOARD_PIN_16, GPIO.RISING, callback=self.PrevChapter_Callback)   
+        GPIO.add_event_detect(GPIO_BOARD_PIN_18, GPIO.RISING, callback=self.NextEpisode_Callback) 
         
         # Create instance of VLC and force window to be top level
-        Instance = vlc.Instance(['--video-on-top'])
+        self.instance = vlc.Instance(['--video-on-top'])
         #Instance = vlc.Instance()        
 
         # Create media player object
-        self.player = Instance.media_player_new()
+        self.player = self.instance.media_player_new()
 
         # Set media player to fullscreen
         self.player.set_fullscreen(True)
+        
+        # Get list of video files
+        self.file_list = os.listdir(VIDEOS_DIRECTORY)
 
-        # Set path to video to be played
-        Media = Instance.media_new("/home/pi/Desktop/Phantom of the Opera (1943).m4v")
+        # Set path to first video to be played
+        self.video_played = 0
+        full_path = VIDEOS_DIRECTORY + "/" + self.file_list[self.video_played]
+        media = self.instance.media_new(full_path)
 
         # Give the media player object the video to be played
-        self.player.set_media(Media)
+        self.player.set_media(media)
         
 
     def SetupGPIO(self):
@@ -89,6 +99,28 @@ class VideoPlayer:
         
         time.sleep(0.25)
         GPIO.add_event_detect(GPIO_BOARD_PIN_16, GPIO.RISING, callback=self.PrevChapter_Callback) 
+        
+    def NextEpisode_Callback(self, channel):
+        GPIO.remove_event_detect(GPIO_BOARD_PIN_18)
+        print("Button was pushed!")
+        
+        # Stop video being played
+        #self.player.pause()
+        
+        # Get next video to be played
+        self.video_played = self.video_played + 1
+        if self.video_played >= len(self.file_list):
+            self.video_played = 0
+        full_path = VIDEOS_DIRECTORY + "/" + self.file_list[self.video_played]
+        media = self.instance.media_new(full_path)
+
+        # Give the media player object the video to be played
+        self.player.set_media(media)
+        
+        #time.sleep(0.25)
+        GPIO.add_event_detect(GPIO_BOARD_PIN_18, GPIO.RISING, callback=self.NextEpisode_Callback) 
+        
+        self.Play()
     
     # Destructor    
     def __del__(self):
@@ -98,13 +130,7 @@ def main():
     videoplayer = VideoPlayer()
     
     videoplayer.Play()
-   
-    # Wait for 10 seconds
-    #time.sleep(10)
-
-    # Set media player to default screen size
-    #player.set_fullscreen(False)    
-    
+     
     # Wait for Enter press
     message = input("Press enter to quit\n\n")
     
